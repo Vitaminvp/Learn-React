eaa27b879a8bc56109b9a46db15f50ce90ebd87b
Vitalii Vitaminenko <vitamin@ukr.net>
2018-08-28 20:21:48 +0300

    home

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound (props){
+    return (
+        <div>
+            <p>Not Found!</p>
+        </div>
+    );
+}
+
+export default NotFound;
--- src/helpers.js
+++ src/helpers.js
@@ -16,9 +16,11 @@ export function slugify(text) {
 }
 
 export function getFunName() {
-  const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
+    const nouns = ['женщины', 'мужчины', 'дети', 'зубы', 'ноги', 'люди', 'листья', 'мыши', 'гуси', 'половинки', 'ножи', 'жены', 'жизни', 'эльфы', 'хлебы', 'картофель', 'томаты', 'кактусы', 'фокусы', 'грибы', 'ядра', 'учебные', 'анализы', 'диагнозы', 'оазисы', 'тезисы', 'кризисы', 'явления', 'критерии', 'данные'];
+    const adjectives = ['очаровательные', 'красивые', 'чистые', 'серые', 'элегантные', 'причудливые', 'гламурные', 'красивые', 'длинные', 'великолепные', 'старомодные', 'простые', 'странные', 'сверкающие', 'уродливые', 'неприглядные', 'сердитые', 'сбитые с толку', 'неуклюжие', 'побежденные', 'смущенные', 'жестокие', 'сердитые', 'беспомощные', 'зудящие', 'ревнивые', 'ленивые', 'таинственные', 'нервные', 'отвратительные', 'панические', 'отталкивающие', 'страшные', 'бездумные', 'недовольные', 'обеспокоенные'];
 
-  const nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
+    // const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
+    // const nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
 
-  return `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
+  return `${rando(adjectives)}-${rando(nouns)}`;
 }
